03e942394be56fca5c0d65fe8804df76ab51367f
goluking5812-lgtm <goluking5812@gmail.com>
2026-05-22 21:19:32 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+res.send("Hindi dubbing app running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+console.log("Server started");
+});
