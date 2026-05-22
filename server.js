41ad34dd767e176c6c9ca69d137645e479d0fc67
goluking5812-lgtm <goluking5812@gmail.com>
2026-05-22 22:03:08 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,28 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-res.send("Hindi dubbing app running");
+res.send(`
+<html>
+<head>
+<title>Hindi Dubbing App</title>
+</head>
+<body style="font-family:sans-serif;text-align:center;padding-top:50px;">
+<h1>Hindi Dubbing App 🎬</h1>
+<p>Chinese Video to Hindi Dubbing</p>
+
+<button style="
+padding:15px 25px;
+font-size:18px;
+background:red;
+color:white;
+border:none;
+border-radius:10px;
+">
+Upload Video
+</button>
+</body>
+</html>
+`);
 });
 
 const PORT = process.env.PORT || 3000;
